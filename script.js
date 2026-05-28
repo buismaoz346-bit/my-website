b04307c4146ec6943aa84019bb517ae85633b259
Syed Maoz Arif <buismaoz346-bit@users.noreply.github.com>
2026-05-28 09:33:57 +0500

    Remove artificial loading screen delay

--- script.js
+++ script.js
@@ -234,13 +234,3 @@ window.addEventListener('touchmove', function(e) {
 }, { passive: false });
 
 
-// --- Loading Screen ---
-window.addEventListener('load', () => {
-    setTimeout(() => {
-        const loader = document.getElementById('loader');
-        if (loader) {
-            loader.style.opacity = '0';
-            setTimeout(() => loader.style.display = 'none', 500);
-        }
-    }, 1000);
-});
