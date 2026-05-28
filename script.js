4fd8a7a796f88aed4eb4cd5ef507d14ab080dc30
Syed Maoz Arif <buismaoz346-bit@users.noreply.github.com>
2026-05-28 09:23:55 +0500

    Remove custom cursor

--- script.js
+++ script.js
@@ -245,25 +245,7 @@ window.addEventListener('load', () => {
     }, 1000);
 });
 
-// --- Custom Cursor ---
-const cursorDot = document.querySelector('.cursor-dot');
-const cursorOutline = document.querySelector('.cursor-outline');
 
-window.addEventListener('mousemove', (e) => {
-    const posX = e.clientX;
-    const posY = e.clientY;
-    
-    if (cursorDot) {
-        cursorDot.style.left = `${posX}px`;
-        cursorDot.style.top = `${posY}px`;
-    }
-    
-    if (cursorOutline) {
-        // Smooth follow
-        cursorOutline.animate({
-            left: `${posX}px`,
-            top: `${posY}px`
-        }, { duration: 500, fill: "forwards" });
     }
 });
 
